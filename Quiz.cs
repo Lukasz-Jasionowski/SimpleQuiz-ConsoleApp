6b74af6362f0daeddc60a061db605f11dbef60e4
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2024-07-18 12:09:37 +0200

    Add code to display quiz questions and answers in Quiz class

--- Quiz.cs
+++ Quiz.cs
@@ -11,7 +11,21 @@ namespace QuizConsoleApp
 
         public void DisplayQuestion(Question question)
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("╔═════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║                                 Question                                ║");
+            Console.WriteLine("╚═════════════════════════════════════════════════════════════════════════╝");
+            Console.ResetColor();
             Console.WriteLine(question.QuestionText);
+
+            for (int i = 0; i < question.Answers.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("   ");
+                Console.Write(i + 1);
+                Console.ResetColor();
+                Console.WriteLine($". {question.Answers[i]}");
+            }
         }
     }
 }
